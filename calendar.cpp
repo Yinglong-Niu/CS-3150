4ee8d7c4116079009d7fcf036054fbe65c9a00c2
MarkYuchengLong <60054888+MarkYuchengLong@users.noreply.github.com>
2021-12-08 22:50:02 -0700

    Update calendar.cpp

--- calendar.cpp
+++ calendar.cpp
@@ -1,4 +1,4 @@
-/* Yucheng Long try to edit
+/* 
  * driver.cpp
  *
  *  Created on: 12/07/2021
@@ -6,28 +6,30 @@
  */
 
 #include "calendar.h"
-#include <iostream>
-using namespace std;
+#include <iostream>	//Provides input and output support for objects of built-in types
+using namespace std;	//Declare that this file uses the C++ standard library
 calendar::calendar()
-		{
+		{		// Default constructor initialize private variables
 			yr = 0;
 			mon = 0;
             		day = 0;
 		}
 
 calendar::calendar(int yr, int mon,int day)
-		{
+		{		// Parameter constructor initialize private variables
 			this->yr = yr;
 			this->mon = mon;
             		this->day = day;
 		}
+				// Overload the input operator
          istream &operator>>(istream  & in,  calendar &x){
        		in>>x.yr>>x.mon>>x.day;
-      		return in;
+      		return in;	// Support continuous reading
        }
+				// Overload the output operator
          ostream &operator<<( ostream &output, const calendar &x )
       {
          	output<<x.yr<<" "<<x.mon<<" "<<x.day;
-         	return output;
+         	return output;	// Support continuous output
       }
 
