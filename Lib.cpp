a26676af01c440fd607fdafe112688f724fe3a5a
Yinglong-Niu <72998662+Yinglong-Niu@users.noreply.github.com>
2021-12-13 15:28:45 -0700

    Update Lib.cpp

--- Lib.cpp
+++ Lib.cpp
@@ -7,7 +7,7 @@
 //============================================================================
 //All of the search method, no matter srach book or search user, I did not know how to print the sentence that not found first time.
 //I did some reserach and I used one person's idea.
-//Reference: https://blog.csdn.net/weixin_44335707/article/details/106829327
+//Reference: https://blog.csdn.net/weixin_44335707/article/details/106829327 line 116-126
 #include <iostream>
 using namespace std;
 #include "info.h"
