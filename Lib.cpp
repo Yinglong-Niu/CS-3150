49e6d4cf9ec57e1256d7fae01d2594698e36ea6e
Yinglong-Niu <72998662+Yinglong-Niu@users.noreply.github.com>
2021-12-13 15:25:50 -0700

    Create Lib.cpp

--- Lib.cpp
+++ Lib.cpp
@@ -5,7 +5,9 @@
 // Copyright   : Your copyright notice
 // Description : Library Management System
 //============================================================================
-
+//All of the search method, no matter srach book or search user, I did not know how to print the sentence that not found first time.
+//I did some reserach and I used one person's idea.
+//Reference: https://blog.csdn.net/weixin_44335707/article/details/106829327
 #include <iostream>
 using namespace std;
 #include "info.h"
